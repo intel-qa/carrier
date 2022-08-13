29466c9f7194758512b7863b28c02ce712aba611
Gaurav Shah <gauravshah.89@gmailcom>
2022-08-13 21:17:29 +0530

    add compose proc

--- src/stdlib_ext.cr
+++ src/stdlib_ext.cr
@@ -3,3 +3,23 @@ struct Int32
     Int32.new(0)
   end
 end
+
+struct Proc
+  def <<(block : Proc(*U, V)) forall U, V
+    Proc(*T, V).new { |arg| call(block.call(arg)) }
+  end
+end
+
+# def add(x : Int32)
+#   x + 1
+# end
+
+# def mult(x : Int32)
+#   x * 9
+# end
+
+# add = ->(x : Int32) { x+ 1 }
+# mult = ->(x : Int32) { x * 10 }
+
+# x = add << mult
+# p! x.call(10)
